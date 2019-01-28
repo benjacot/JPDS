0787d9db3a46df479894981ae2f299c782e9adc1
benjacot <jacotb@wwu.edu>
2019-01-28 12:50:23 -0800

    updated selectionSort, still bad code

--- SelectionSort/selectionSort.java
+++ SelectionSort/selectionSort.java
@@ -1,30 +1,29 @@
 class selectionSort {
+
   public static void main(String[] args) {
     int[] A = {17, 4, 9, 3, 5, 6, 11, 20};
-
+    selectionSort(A);
   }
 
-  public static int[] selectionSort(int[] A) {
-    // initialize currentMin, set to first value in array
-    int currentMin = A[0];
-    // set up boolean to run while loop
-    boolean unsorted = true;
-    while (unsorted) {
-      // initiate gates to check whether array is unsorted
-      for (int i = 0; i < A.length; i++) {
-        if (A[i] < currentMin) {
-          break;
+  public static void selectionSort(int[] A) {
+    // set for loop to iterate over A
+    for (int i = 0; i < A.length; i++) {
+      // set first unsorted element as minimum
+      int currentMin = A[i];
+      // initialize for loop to check for true currentMin
+      // initialize mindIndex to keep location of currentMin
+      int minIndex = i;
+      for (int j = i + 1; j < A.length; i++) {
+        if (A[j] < currentMin) {
+          minIndex = j;
+          currentMin = A[j];
         }
-        
       }
+      // swap currentMin with first unsorted position
+      int swapValue = A[i];
+      A[i] = currentMin;
+      A[minIndex] = swapValue;
     }
-
-    for (int i = 1; i < A.length; i++) {
-      if (A[i] < currentMin) {
-        currentMin = A[i];
-      }
-    }
-
-    return A;
   }
+
 }
