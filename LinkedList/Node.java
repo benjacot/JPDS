6b8b19bbec547dd0caae2e4a1b3c1ad1e6b3f0ae
benjacot <jacotb@wwu.edu>
2019-02-10 20:54:47 -0800

    Node.java early stage

---
+++ LinkedList/Node.java
@@ -0,0 +1,12 @@
+class Node {
+  /* 2 attributes: data for int value in node,
+  * next to point to next node */
+  int data;
+  Node next;
+
+  // constructor for creating object nodes and assigning values to them
+  Node(int data) {
+    this.data = data;
+  }
+  
+}
